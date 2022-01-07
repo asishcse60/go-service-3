a0cd86fc6521801539ef8467c889bd69d837c0fe
asish_orbitax <asish.pal@orbitax.com>
2022-01-08 00:19:24 +0600

    sending response

--- app/services/sales-api/handlers/v1/testgrp/testgrp.go
+++ app/services/sales-api/handlers/v1/testgrp/testgrp.go
@@ -2,7 +2,7 @@ package testgrp
 
 import (
 	"context"
-	"encoding/json"
+	"github.com/asishcse60/service/foundation/web"
 	"net/http"
 
 	"go.uber.org/zap"
@@ -24,5 +24,5 @@ func (h Handlers) Test(ctx context.Context, w http.ResponseWriter, r *http.Reque
 	statusCode := http.StatusOK
 	h.Log.Infow("readiness", "statusCode", statusCode, "method", r.Method, "path", r.URL.Path, "remoteaddr", r.RemoteAddr)
 
-	return json.NewEncoder(w).Encode(status)
+	return web.Respond(ctx,w, status, statusCode)
 }
--- foundation/web/web.go
+++ foundation/web/web.go
@@ -16,13 +16,15 @@ type Handler func(ctx context.Context, w http.ResponseWriter, r *http.Request) e
 type App struct {
 	*httptreemux.ContextMux
 	shutdown chan os.Signal
+	mw       []Middleware
 }
 
 // NewApp - creates an App value that handle a set of routes for the application.
-func NewApp(shutdown chan os.Signal) *App {
+func NewApp(shutdown chan os.Signal, mw ...Middleware) *App {
 	return &App{
 		ContextMux: httptreemux.NewContextMux(),
 		shutdown:   shutdown,
+		mw:         mw,
 	}
 }
 
@@ -31,17 +33,24 @@ func (a *App) SignalShutdown() {
 	a.shutdown <- syscall.SIGTERM
 }
 
-func (a *App) Handle(method, group, path string, handler Handler) {
+func (a *App) Handle(method, group, path string, handler Handler, mw ...Middleware) {
+
+	// First wrap handler specific middleware around this handler.
+	handler = wrapMiddleware(mw, handler)
+
+	// Add the application's general middleware to the handler chain.
+	handler = wrapMiddleware(a.mw, handler)
+
 	h := func(w http.ResponseWriter, r *http.Request) {
 		// PRE CODE PROCCESSING.
-		if err := handler(r.Context(), w, r); err != nil{
+		if err := handler(r.Context(), w, r); err != nil {
 
 		}
 		// POST CODE PROCCESSING.
 	}
 
 	finalPath := path
-	if group != ""{
+	if group != "" {
 		finalPath = "/" + group + path
 	}
 	// original call to library.
